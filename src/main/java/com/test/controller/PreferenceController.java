644399f413610e607ed5718ec407cf1b1195af6f
andrii_kliui <andrii.kliui@fabware.com>
2017-02-16 12:39:04 +0200

    html is work

---
+++ src/main/java/com/test/controller/PreferenceController.java
@@ -0,0 +1,27 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PreferenceController {
+
+    @RequestMapping(value={"", "/"})
+    public String home() {
+
+
+        return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+
+        return "hello";
+    }
+
+}
