76d88863b1bdf866090fb75dbf86c2886766079f
andrii_kliui <andrii.kliui@fabware.com>
2017-02-16 19:14:27 +0200

    add Thymeleaf

--- src/main/java/com/test/controller/PreferenceController.java
+++ src/main/java/com/test/controller/PreferenceController.java
@@ -2,6 +2,8 @@ package com.test.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PreferenceController {
@@ -9,19 +11,22 @@ public class PreferenceController {
     @RequestMapping(value={"", "/"})
     public String home() {
 
-
         return "home";
     }
 
     @RequestMapping("/login")
     public String login() {
+
         return "login";
     }
 
     @RequestMapping("/hello")
-    public String hello() {
+    public ModelAndView hello(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("hello");
 
-        return "hello";
+        mav.addObject("message", "- Hello, page -");
+        return mav;
     }
 
 }
